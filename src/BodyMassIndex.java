bb1bbca12c977a2120b42e09593648b2110db13c
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-23 14:22:05 +0300

    Update BodyMassIndex.java

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class BodyMassIndex {
+
+import java.util.Scanner;
+public class BodyMassIndex {     public static void main(String[] args) {
+
+    //vucut-bot indexi
+    double kilo, boy, kitleIndexı;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("LUTFEN BOYUNUZU GIRINIZ = ");
+    boy = input.nextDouble();
+
+    System.out.print("LUTFEN KILONUZU GIRINIZ = ");
+    kilo = input.nextDouble();
+
+    kitleIndexı =( kilo /( boy * boy));
+
+    System.out.println("VUCUT KITLE INDEXINIZ = " + kitleIndexı);
+
+
+
+
+}
+
 }
